b8d3cad32111bb5eeecb55fd890b6499ce7e519b
Jonathan25J <ik.ben.jona4@gmail.com>
2024-12-26 14:41:32 +0100

    chest images are now loaded from the archive

--- Events/chests.py
+++ Events/chests.py
@@ -1,7 +1,7 @@
 import discord
-from discord.ext import commands
 import random
-
+from discord.ext import commands
+from utils import images_folder, get_image
 
 async def setup(client):
     await client.add_cog(chests(client))
@@ -15,53 +15,49 @@ class chests(commands.Cog):
 
 async def getChests(interaction):
     golden_chest = discord.Embed(title="Golden Chest", color=0x499fc0)
-    golden_chest.set_thumbnail(
-        url="https://images-ext-2.discordapp.net/external/7fgMPYAXw0eoEyL-ttynfi4mtIFNL3gM1zdmH3Grhjo/%3Fcb"
-            "%3D20201020025220/https/static.wikia.nocookie.net/clashroyale/images/8/8b/GoldenChest.png/revision"
-            "/latest")
+    thumbnail = get_image(f'{images_folder}chests\\golden_chest.png')
+    golden_chest.set_thumbnail(url=f'attachment://{thumbnail.filename}')
     golden_chest.add_field(name="Bet", value="50 HP", inline=True)
     golden_chest.add_field(name="Win multipliers", value="2x", inline=True)
     golden_chest.add_field(name="Lose chances", value="75%", inline=True)
     view = golden_chest_v()
-    await interaction.user.send(embed=golden_chest, view=view)
+    await interaction.user.send(embed=golden_chest, view=view, file=thumbnail)
 
     kingchest = discord.Embed(title="King's Chest", color=0x2e3b67)
-    kingchest.set_thumbnail(
-        url="https://static.wikia.nocookie.net/clashroyale/images/5/51/Kings_Chest.png/revision/latest/scale-to-width"
-            "-down/120?cb=20171211182109")
+    thumbnail = get_image(f'{images_folder}chests\\kings_chest.png')
+    kingchest.set_thumbnail(url=f'attachment://{thumbnail.filename}')
     kingchest.add_field(name="Bet", value="100 HP", inline=True)
     kingchest.add_field(name="Win multipliers", value="2x", inline=True)
     kingchest.add_field(name="Lose chances", value="65%", inline=True)
     view = king_chest_v()
-    await interaction.user.send(embed=kingchest, view=view)
+    await interaction.user.send(embed=kingchest, view=view, file=thumbnail)
 
     magical_chest = discord.Embed(title="Magical Chest", color=0xff62ff)
-    magical_chest.set_thumbnail(
-        url="https://static.wikia.nocookie.net/clashroyale/images/9/93/MagicalChest.png/revision/latest?cb=20160312171354")
+    thumbnail = get_image(f'{images_folder}chests\\magical_chest.png')
+    magical_chest.set_thumbnail(url=f'attachment://{thumbnail.filename}')
     magical_chest.add_field(name="Bet", value="150 HP", inline=True)
     magical_chest.add_field(name="Win multipliers", value="2x 3x", inline=True)
     magical_chest.add_field(name="Lose chances", value="55%", inline=True)
     view = magical_chest_v()
-    await interaction.user.send(embed=magical_chest, view=view)
+    await interaction.user.send(embed=magical_chest, view=view, file=thumbnail)
 
     mega_lightning_chest = discord.Embed(title="Mega Lightning Chest", color=0x8dc9f7)
-    mega_lightning_chest.set_thumbnail(
-        url="https://www.deckshop.pro/img/chests/mlc.png")
+    thumbnail = get_image(f'{images_folder}chests\\mega_lightning_chest.png')
+    mega_lightning_chest.set_thumbnail(url=f'attachment://{thumbnail.filename}')
     mega_lightning_chest.add_field(name="Bet", value="300 HP", inline=True)
     mega_lightning_chest.add_field(name="Win multipliers", value="2x 3x", inline=True)
     mega_lightning_chest.add_field(name="Lose chances", value="45%", inline=True)
     view = mega_lightning_chest_v()
-    await interaction.user.send(embed=mega_lightning_chest, view=view)
+    await interaction.user.send(embed=mega_lightning_chest, view=view, file=thumbnail)
 
     legendary_kings_chest = discord.Embed(title="Legendary King's Chest", color=0x6ea3b9)
-    legendary_kings_chest.set_thumbnail(
-        url="https://static.wikia.nocookie.net/clashroyale/images/4/42/Legendary_Kings_Chest.png/revision/latest?cb"
-            "=20171215191757")
+    thumbnail = get_image(f'{images_folder}chests\\legendary_kings_chest.png')
+    legendary_kings_chest.set_thumbnail(url=f'attachment://{thumbnail.filename}')
     legendary_kings_chest.add_field(name="Bet", value="600 HP", inline=True)
     legendary_kings_chest.add_field(name="Win multipliers", value="2x 3x 4x", inline=True)
     legendary_kings_chest.add_field(name="Lose chances", value="40%", inline=True)
     view = legendary_kings_chest_v()
-    await interaction.response.send_message(embed=legendary_kings_chest, view=view)
+    await interaction.response.send_message(embed=legendary_kings_chest, view=view, file=thumbnail)
 
 
 class golden_chest_v(discord.ui.View):
